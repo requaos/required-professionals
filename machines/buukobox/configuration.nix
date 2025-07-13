bbb47b7017a75d950b5d5118c69b7b5bec5f6905
Neil Skinner <reqpro@requaos.com>
2025-07-13 15:31:13 -0400

    little bits

--- machines/buukobox/configuration.nix
+++ machines/buukobox/configuration.nix
@@ -265,21 +265,6 @@ in {
       thunderbolt
       # Bluetooth
       bluez
-      # Audio
-      pipewire
-      easyeffects
-      # pw-viz <-- was cool eGui native rust graph vizualizer, but it broke and isn't
-      # maintained, so we should just write lua scripts to auto-set our graphs without
-      # needing a visual interface.
-      # see buukubox environment.nix for lua script example for setting graph rules on boot:
-      # use `pw-cli ls` to list the current graph for references in your lua scripts.
-      wireplumber
-      # utilities to support pulseaudio stuff under pipewire
-      pulseaudio
-      pamixer
-      pavucontrol
-      # alsa stuff
-      alsa-utils
 
       # coms
       mumble
--- modules/sound/pipewire.nix
+++ modules/sound/pipewire.nix
@@ -4,6 +4,22 @@
   config,
   ...
 }: {
+  environment.systemPackages = with pkgs; [
+    pipewire
+    easyeffects
+    # pw-viz <-- was cool eGui native rust graph vizualizer, but it broke and isn't
+    # maintained, so we should just write lua scripts to auto-set our graphs without
+    # needing a visual interface.
+    # see buukubox environment.nix for lua script example for setting graph rules on boot:
+    # use `pw-cli ls` to list the current graph for references in your lua scripts.
+    wireplumber
+    # utilities to support pulseaudio stuff under pipewire
+    pulseaudio
+    pamixer
+    pavucontrol
+    # alsa stuff
+    alsa-utils
+  ];
   services = {
     pipewire = {
       enable = true;
